88929d161047d8f96be654c16259dce39d1e5323
sysadmin <sysadmin@bcalab1-m05>
2021-04-07 11:38:25 +0530

    first commite

---
+++ company/src/company/Employee.java
@@ -0,0 +1,18 @@
+package company;
+
+public class Employee {
+	String name; 
+	String dept; 
+	Employee (String name, String dept) 
+	{ 
+	this.name = name; 
+	this.dept = dept; 
+	} 
+
+
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+
+	}
+
+}
